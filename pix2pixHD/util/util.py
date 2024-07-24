6325e12e478df5eb44b4a926d1cdeeffce8ee15b
Samuel <smtonks2712@gmail.com>
2024-07-24 17:57:00 +0100

    update tensor2ims

--- pix2pixHD/util/util.py
+++ pix2pixHD/util/util.py
@@ -39,8 +39,10 @@ def tensors2ims(opt, image_tensors, imtype=np.uint16, normalize=True, stack_pred
     elif imtype == "dlmbl":
         if opt.target == "nuclei":
             image_numpy = (image_numpy * 1513) + 1407
+            image_numpy.astype(np.float32)
         elif opt.target == "cyto":
             image_numpy = (image_numpy * 5309) + 10274
+            image_numpy.astype(np.float32)
         else:
             raise ValueError("Unknown target")
     else:
@@ -49,7 +51,7 @@ def tensors2ims(opt, image_tensors, imtype=np.uint16, normalize=True, stack_pred
         else:
             image_numpy = np.transpose(image_numpy, (1, 2, 0))
             # image_numpy = np.clip(image_numpy, 0, 1)
-    return image_numpy.astype(imtype)
+    return image_numpy
 
 
 
