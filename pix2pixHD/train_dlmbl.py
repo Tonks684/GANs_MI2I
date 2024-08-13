95a7137b8558f5e6282988e60ec3c84f5fe27329
Samuel <smtonks2712@gmail.com>
2024-08-13 22:35:36 +0100

    tensors2imgs

--- pix2pixHD/train_dlmbl.py
+++ pix2pixHD/train_dlmbl.py
@@ -155,7 +155,6 @@ def val_epoch(opt, model, dataset_val):
             running_loss_G_GAN += loss_G_GAN
             running_loss_G_GAN_Feat += loss_G_GAN_Feat
             running_loss_G_VGG += loss_G_VGG
-            
         ### display output images
         input_data = util.tensors2ims(opt, data['label'],imtype='dlmbl')
         virtual_stain = util.tensors2ims(opt, generated.data,imtype='dlmbl')
