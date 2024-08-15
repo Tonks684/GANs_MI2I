28f75a0e3b4a4867fbb8e074d4fa0f1524e8137c
Samuel <smtonks2712@gmail.com>
2024-08-15 16:38:44 +0100

    enable sampling and inference runs

--- pix2pixHD/test_dlmbl.py
+++ pix2pixHD/test_dlmbl.py
@@ -24,14 +24,14 @@ def inference(dataset, opt, model):
     for data in tqdm(dataset):
         generated = model.inference(data['label'], data['inst'], data['image'])
         img_path = data['path']
-        visuals = OrderedDict([('synthesized_image', util.tensor2im(opt,generated.data[0],imtype="dlmbl",normalize=False))])
+        # Unnormalize the image
+        generated = util.tensor2im(opt,generated.data[0],imtype="dlmbl",normalize=False)
+        visuals = OrderedDict([('synthesized_image',generated )])
         img_name = img_path[0].split('/')[-1]
         save_path_pred = os.path.join(
                 opt.results_dir, img_name)
-        save_path_real = os.path.join(
-                opt.results_dir, img_path[0].split('/')[-1][:-5] + '_real.tiff')
         imsave(save_path_pred, visuals['synthesized_image'].astype(np.float32),imagej=True)
-        imsave(save_path_real,util.tensor2im(opt,data['image'],imtype='dlmbl',normalize=False),imagej=True)
+        
 def sampling(dataset, opt, model):
     """
     Perform sampling on the given dataset using the specified model.
@@ -45,25 +45,16 @@ def sampling(dataset, opt, model):
         None
     """
     for data in tqdm(dataset):
+        stack_pred = np.zeros((opt.variational_inf_runs,512,512))
         for sample in range(opt.variational_inf_runs):
-            for data in tqdm(dataset):
-                generated = model.inference(data['label'], data['inst'], data['image'])
-                generated.cpu()
-                if opt.output_reshape:
-                    prediction = util.tensor2im(generated.data[0], imtype=np.uint16)
-                    prediction = cv2.resize(
-                        prediction, (opt.output_reshape, opt.output_reshape),
-                        interpolation=cv2.INTER_LINEAR)
-                    input_label = util.tensor2label(data['label'][0], opt.label_nc)
-                    input_label = transform.resize(
-                        input_label, (opt.output_reshape, opt.output_reshape))
-                    visuals = OrderedDict([('input_label', input_label),('synthesized_image', prediction)])
-                else:
-                    visuals = OrderedDict([('input_label',util.tensor2label(data['label'][0], opt.label_nc)),('synthesized_image', util.tensor2im(generated.data[0],imtype='dlmbl',normalize=False))])
-                img_path = data['path']
-                print('Processing image... %s' % img_path)
-                img_name = img_path[0].split('/')[-1]
-                image_name = f"img_name_{sample}.tiff"
-                save_path = os.path.join(
-                        opt.results_dir, img_name)
-                imsave(save_path, visuals['synthesized_image'].astype(np.float32),imagej=True)
+            generated = model.inference(data['label'], data['inst'], data['image'])
+            generated = generated.cpu().detach()
+            visuals = OrderedDict([('input_label',util.tensor2label(data['label'][0], opt.label_nc)),('synthesized_image', util.tensor2im(generated.data[0],imtype='dlmbl',normalize=False))])
+            stack_pred[sample] = visuals['synthesized_image']
+
+        img_path = data['path']
+        img_name = img_path[0].split('/')[-1][:-5]
+        samples_name = f"{img_name}_samples.tiff"
+        save_path = os.path.join(
+                opt.results_dir, samples_name)
+        imsave(save_path, stack_pred.astype(np.float32),imagej=True)
